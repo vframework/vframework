156064daba8e1bb6409636731a516b84f5e8fea9
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 11:12:52 +0800

    Create ItemDeleteEventDetails.php

---
+++ src/app/Cache/ItemDeleteEventDetails.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * V Framework
+ * http://vframework.cn
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace VFramework\App\Cache;
+
+/**
+ * @property string $key
+ */
+class ItemDeleteEventDetails
+{
+
+    use \Duzhenye\DataObjectTrait;
+
+    /**
+     * 
+     * @param string $key
+     */
+    public function __construct(string $key)
+    {
+        $this
+                ->defineProperty('key', [
+                    'type' => 'string'
+                ])
+        ;
+        $this->key = $key;
+    }
+
+}
