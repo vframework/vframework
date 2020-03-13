75dfdcc21e05626e22e7c7fa5084646b94fc31cf
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 11:13:09 +0800

    Create ItemExistsEventDetails.php

---
+++ src/app/Cache/ItemExistsEventDetails.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * V Framework
+ * http://vframework.cn
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace VFramework\App\Cache;
+
+/**
+ * @property string $key
+ * @property bool $exists
+ */
+class ItemExistsEventDetails
+{
+
+    use \Duzhenye\DataObjectTrait;
+
+    /**
+     * @param string $key
+     * @param bool $exists
+     */
+    public function __construct(string $key, bool $exists)
+    {
+        $this
+                ->defineProperty('key', [
+                    'type' => 'string'
+                ])
+                ->defineProperty('exists', [
+                    'type' => 'bool'
+                ])
+        ;
+        $this->key = $key;
+        $this->exists = $exists;
+    }
+
+}
