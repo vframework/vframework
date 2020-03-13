c6ca653ed03cb016e6d02593491d6fd0acadfe01
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 10:54:59 +0800

    Add files via upload

---
+++ src/Internal/ErrorHandler.php
@@ -0,0 +1,164 @@
+<?php
+
+/*
+ * V Framework
+ * http://vframework.cn
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace VFramework\Internal;
+
+use VFramework\App;
+
+/**
+ * The default error handler.
+ * @internal
+ */
+class ErrorHandler
+{
+
+    /**
+     * 
+     * @param \VFramework\App $app
+     * @param \Throwable $exception
+     * @param array $options
+     * @return void No value is returned.
+     */
+    static function handleException(\VFramework\App $app, \Throwable $exception, array $options): void
+    {
+        $errors = [];
+        while ($exception instanceof \Exception || $exception instanceof \Error) {
+            $message = 'Exception: ' . $exception->getMessage() . ' in ' . $exception->getFile() . ':' . $exception->getLine();
+            $trace = $exception->getTrace();
+            $errors[] = ['message' => $message, 'trace' => $trace];
+            $exception = $exception->getPrevious();
+        }
+        $errors = array_reverse($errors);
+        self::handleErrors($app, $errors, $options);
+    }
+
+    /**
+     * 
+     * @param \VFramework\App $app
+     * @param array $errorData
+     * @param array $options
+     * @return void No value is returned.
+     */
+    static function handleFatalError(\VFramework\App $app, array $errorData, array $options): void
+    {
+        $messageParts = explode(' in ' . $errorData['file'] . ':' . $errorData['line'], $errorData['message'], 2);
+        $message = 'Fatal error: ' . trim($messageParts[0]) . ' in ' . $errorData['file'] . ':' . (int) $errorData['line'];
+        $trace = isset($messageParts[1]) ? [trim(str_replace('Stack trace:', '', $messageParts[1]))] : [];
+        self::handleErrors($app, [['message' => $message, 'trace' => $trace]], $options);
+    }
+
+    /**
+     * 
+     * @param \VFramework\App $app
+     * @param array $errors
+     * @param array $options
+     * @return void No value is returned.
+     */
+    static private function handleErrors(\VFramework\App $app, array $errors, $options): void
+    {
+        $level = ob_get_level();
+        for ($i = 0; $i < $level; $i++) {
+            ob_end_clean();
+        }
+
+        $logErrors = isset($options['logErrors']) && (bool) $options['logErrors'] === true;
+        $displayErrors = isset($options['displayErrors']) && (bool) $options['displayErrors'] === true;
+
+        if ($logErrors || $displayErrors) {
+            if ($logErrors) {
+                $logKey = uniqid();
+            }
+            for ($i = 0; $i < 3; $i++) {
+                if (!$logErrors && $i !== 2) {
+                    continue;
+                }
+                if (!$displayErrors && $i === 2) {
+                    continue;
+                }
+
+                $isSimpleLog = $i === 0 || $i === 2;
+                $log = '';
+                $addContent = function (string $title, string $data) use (&$log) {
+                    if (isset($log[0])) {
+                        $log .= "\n\n";
+                    }
+                    $log .= $title . ':' . (isset($data[0]) ? "\n" . trim($data) : '');
+                };
+                if ($logErrors && $isSimpleLog) {
+                    $addContent('Log key', $logKey);
+                }
+
+                $errorsCount = sizeof($errors);
+                foreach ($errors as $j => $error) {
+                    $message = isset($error['message']) ? $error['message'] : '';
+                    $trace = isset($error['trace']) ? $error['trace'] : [];
+
+                    $simpleTrace = [];
+                    foreach ($trace as $traceRow) {
+                        $simpleTrace[] = (isset($traceRow['file']) ? $traceRow['file'] . ':' . (isset($traceRow['line']) ? $traceRow['line'] : '') : '...');
+                    }
+                    if ($errorsCount > 1) {
+                        $addContent('Error ' . ($j + 1) . ' of ' . $errorsCount, '');
+                    }
+                    $addContent('Message', $message);
+                    $addContent('Simple trace', implode("\n", $simpleTrace));
+                    if (!$isSimpleLog) {
+                        $addContent('Full trace', print_r($trace, true));
+                    }
+                }
+
+                $addContent('Request', $app->request->method . ' ' . $app->request->base . $app->request->path);
+                if (!$isSimpleLog) {
+                    $addContent(
+                        'PHP variables',
+                        print_r(
+                            [
+                                'GET' => $_GET,
+                                'POST' => $_POST,
+                                'SERVER' => $_SERVER
+                            ],
+                            true
+                        )
+                    );
+                }
+
+                if ($i !== 2) {
+                    if ($logErrors) {
+                        try {
+                            if ($isSimpleLog) {
+                                $app->logs->log('error', $log);
+                            } else {
+                                $app->logs->log('error-' . $logKey, $log);
+                            }
+                        } catch (\Exception $e) { }
+                    }
+                } else {
+                    if ($displayErrors) {
+                        http_response_code(503);
+                        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+                        header("Pragma: no-cache");
+                        echo (($errorsCount > 1) ? $errorsCount . ' errors occurred:' : 'Error occurred:') . "\n\n";
+                        echo $log;
+                        exit;
+                    }
+                }
+            }
+        }
+
+        try {
+            $app->send(new App\Response\TemporaryUnavailable());
+        } catch (\Exception $e) {
+            http_response_code(503);
+            header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+            header("Pragma: no-cache");
+            echo 'Temporarily Unavailable';
+        }
+        exit;
+    }
+}
---
+++ src/Internal/Utilities.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * V Framework
+ * http://vframework.cn
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace VFramework\Internal;
+
+/**
+ * 
+ * @internal
+ */
+class Utilities
+{
+
+    /**
+     * 
+     */
+    static $registeredAddons = [];
+
+    /**
+     *
+     * @var array 
+     */
+    static $cache = [];
+
+    /**
+     * 
+     */
+    static function normalizePath(string $path): string
+    {
+        if ($path === '') {
+            return '';
+        }
+        $cacheKey = 'normalize-path-' . $path;
+        if (!isset(self::$cache[$cacheKey])) {
+            for ($i = 0; $i < 2; $i++) {
+                if (strpos($path, '://') !== false) { // is url
+                    $temp = explode('://', $path, 2);
+                    $root = $temp[0] . '://';
+                    $path = $temp[1];
+                    break;
+                } elseif ($path[0] === '/') { // is absolute on Linux
+                    $root = '/';
+                    $path = substr($path, 1);
+                    break;
+                } elseif (preg_match('/^[A-Za-z]:[\/\\\]/i', $path) === 1) { // is absolute on Windows
+                    $root = substr($path, 0, 2) . '/';
+                    $path = substr($path, 3);
+                    break;
+                } else {
+                    if ($i === 0) { // is relative
+                        $path = getcwd() . '/' . $path;
+                    } else {
+                        throw new \Exception('Cannot find absolute path for "' . $path . '"!');
+                    }
+                }
+            }
+            $path = str_replace('\\', '/', $path);
+            for ($i = 0; $i < 100000; $i++) {
+                $newPath = str_replace(['/./', '//'], '/', $path);
+                if (substr($newPath, 0, 2) === './') {
+                    $newPath = substr($newPath, 2);
+                }
+                if (substr($newPath, -2) === '/.') {
+                    $newPath = substr($newPath, 0, -2);
+                }
+                if ($newPath !== $path) {
+                    $path = $newPath;
+                } else {
+                    break;
+                }
+            }
+            if (strpos($path, '..') !== false) {
+                $parts = explode('/', $path);
+                $temp = [];
+                foreach ($parts as $part) {
+                    if ($part === '..') {
+                        array_pop($temp);
+                    } else {
+                        $temp[] = $part;
+                    }
+                }
+                $path = implode('/', $temp);
+            }
+            $result = $root . $path;
+            self::$cache[$cacheKey] = $result;
+            self::$cache['normalize-path-' . $result] = $result; // cache to optimize normalizing normalized paths
+            return $result;
+        }
+        return self::$cache[$cacheKey];
+    }
+}
