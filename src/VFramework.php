2ed8a1f3b83e11fcf3fe0a4c7f4afc8ac950e096
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 10:50:03 +0800

    Create VFramework.php

---
+++ src/VFramework.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * V Framework
+ * http://vframework.cn
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+/**
+ * 
+ */
+class VFramework
+{
+
+    /**
+     * The current V Framework version.
+     * 
+     * @var string
+     */
+    const VERSION = '1.0.0';
+
+}
