061a3e97d83e92240649be715beeeab0716a682b
duzhenye <60967905+duzhenye@users.noreply.github.com>
2020-03-13 10:57:00 +0800

    Add files via upload

---
+++ src/DataList/SliceAction.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * V Framework
+ * http://vframework.cn
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace VFramework\DataList;
+
+/**
+ * 
+ */
+class SliceAction
+{
+
+    use \Duzhenye\DataListSliceActionTrait;
+}
---
+++ src/DataList/SortByAction.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * V Framework
+ * http://vframework.cn
+ * Copyright (c) Duzhenye
+ * Free to use under the MIT license.
+ */
+
+namespace VFramework\DataList;
+
+/**
+ * 
+ */
+class SortByAction
+{
+
+    use \Duzhenye\DataListSortByActionTrait;
+}
